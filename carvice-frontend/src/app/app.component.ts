81c1e77a4ec0f6f39773d678742b2419ff92edfe
bilgeryahov <bayahov1@gmail.com>
2020-04-07 13:31:52 +0300

    create a sidebar

--- carvice-frontend/src/app/app.component.ts
+++ carvice-frontend/src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'carvice-frontend';
+  // data fields
+  private _sidebarOpenStyle: Object = {
+    'width': '100%',
+    'fontSize': '40px',
+    'paddingTop': '10%',
+    'display': 'block'
+  };
+
+  private _sidebarClosedStyle: Object = {
+    'display': 'none'
+  };
+
+  private _sidebarVisible: boolean = false;
+
+  // methods
+  public setSidebarVisibility(): Object {
+    if (this._sidebarVisible) {
+      return this._sidebarOpenStyle;
+    }
+    return this._sidebarClosedStyle;
+  }
+
+  public openSidebar(): void {
+    this._sidebarVisible = true;
+  }
+
+  public closeSidebar(): void {
+    this._sidebarVisible = false;
+  }
 }
