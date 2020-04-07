6d3d76bcd03daaa292a91cff4d503889554cb8d2
bilgeryahov <bayahov1@gmail.com>
2020-04-07 19:03:55 +0300

    based on auth state, determine which nav items will be displayed

--- carvice-frontend/src/app/app.component.ts
+++ carvice-frontend/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  public isAuth = () => this._authService.isAuth();
+
+  constructor(private _authService: AuthService) { }
+}
