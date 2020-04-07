abd459d2d9907edd0778b0d50297e99180dbd0ad
bilgeryahov <bayahov1@gmail.com>
2020-04-07 15:03:53 +0300

    remove any logic from AppComponent

--- carvice-frontend/src/app/app.component.ts
+++ carvice-frontend/src/app/app.component.ts
@@ -5,34 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  // data fields
-  private _sidebarOpenStyle: Object = {
-    'width': '100%',
-    'fontSize': '40px',
-    'paddingTop': '10%',
-    'display': 'block'
-  };
-
-  private _sidebarClosedStyle: Object = {
-    'display': 'none'
-  };
-
-  private _sidebarVisible: boolean = false;
-
-  // methods
-  public setSidebarVisibility(): Object {
-    if (this._sidebarVisible) {
-      return this._sidebarOpenStyle;
-    }
-    return this._sidebarClosedStyle;
-  }
-
-  public openSidebar(): void {
-    this._sidebarVisible = true;
-  }
-
-  public closeSidebar(): void {
-    this._sidebarVisible = false;
-  }
-}
+export class AppComponent { }
