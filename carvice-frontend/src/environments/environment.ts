8737f64f3e0beb5a1fa6b5604167502cadf21747
bilgeryahov <bayahov1@gmail.com>
2020-04-08 14:41:39 +0300

    set-up firebase

--- carvice-frontend/src/environments/environment.ts
+++ carvice-frontend/src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyA_dTR2V8gFSObc0bS2Za6qCckvyy3JHIg",
+  authDomain: "carvice-b21b7.firebaseapp.com",
+  databaseURL: "https://carvice-b21b7.firebaseio.com",
+  projectId: "carvice-b21b7",
+  storageBucket: "carvice-b21b7.appspot.com",
+  messagingSenderId: "590443362599",
+  appId: "1:590443362599:web:38a50f700cc7dc58f15102"
 };
 
 /*
