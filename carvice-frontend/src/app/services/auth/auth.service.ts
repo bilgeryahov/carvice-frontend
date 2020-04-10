e20c7239c2b80c04470fe4a19e08dd7a34869ec8
bilgeryahov <bayahov1@gmail.com>
2020-04-10 12:39:15 +0300

    initial version of auth service

--- carvice-frontend/src/app/services/auth/auth.service.ts
+++ carvice-frontend/src/app/services/auth/auth.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { User } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  // properties
-  public isAuth = () => false;
+export class AuthService implements OnInit {
+  // data fields
+  private _user: User = null;
 
-  constructor(private _firebaseAuth: AngularFireAuth) { }
+  // properties
+  public get isAuth(): boolean {
+    return this._user !== null;
+  }
+
+  public get user(): User {
+    return this._user;
+  }
+
+  constructor(
+    private _firebaseAuth: AngularFireAuth,
+    private _router: Router
+  ) { }
+
+  public ngOnInit(): void {
+    this._firebaseAuth.authState.subscribe((firebaseUser: User) => {
+      if (firebaseUser) {
+        this._user = firebaseUser;
+        this._router.navigate(['/dashboard']);
+        return;
+      }
+      this._user = null;
+      this._router.navigate(['/home']);
+    });
+  }
 }
