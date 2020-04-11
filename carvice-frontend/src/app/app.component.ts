016ef13d505a385f03b891e2992c0e7d7c9d2607
bilgeryahov <bayahov1@gmail.com>
2020-04-11 17:01:38 +0300

    implement basic alerting

--- carvice-frontend/src/app/app.component.ts
+++ carvice-frontend/src/app/app.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
-import { AuthService } from './services/auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { IAlert } from './interfaces/IAlert';
+import { AlertService } from './services/alert.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private _alertMsg: IAlert = null;
+
+  public get alertMsg(): IAlert {
+    return this._alertMsg;
+  }
+
   public get isAuth(): boolean {
     return this._authService.isAuth
   }
 
-  constructor(private _authService: AuthService) { }
+  public get alertPanelClass(): Object {
+    return {
+      'w3-panel w3-round w3-card-4': true,
+      'w3-green': this._alertMsg && this._alertMsg.success,
+      'w3-orange': this._alertMsg && !this._alertMsg.success
+    }
+  }
+
+  constructor(
+    private _authService: AuthService,
+    private _alertService: AlertService
+  ) { }
+
+  public ngOnInit(): void {
+    this._alertService.subject.subscribe((alertMsg: IAlert) => {
+      this._alertMsg = alertMsg;
+      setTimeout(() => this._alertMsg = null, 3000);
+    });
+  }
 }
