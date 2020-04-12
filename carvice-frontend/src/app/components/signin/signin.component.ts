91400d6ce96e55aa9d12686dd6ce9d8e67f20732
bilgeryahov <bayahov1@gmail.com>
2020-04-12 17:44:08 +0300

    work out the 'signin' component

--- carvice-frontend/src/app/components/signin/signin.component.ts
+++ carvice-frontend/src/app/components/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  private _signInForm: FormGroup = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  public get signInForm(): FormGroup {
+    return this._signInForm;
   }
 
+  constructor(
+    private _authService: AuthService
+  ) { }
+
+  public ngOnInit() {
+    this._signInForm = new FormGroup({
+      email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+      password: new FormControl('', { validators: [Validators.required] })
+    });
+  }
+
+  public onSignIn(): void {
+    this._authService.signIn(this._signInForm.value.email, this._signInForm.value.password);
+  }
 }
