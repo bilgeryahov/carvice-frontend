50c0ba03aca307cc41303cc4d05a5d23b5246a62
bilgeryahov <bayahov1@gmail.com>
2020-05-08 11:17:24 +0300

    pages on the shared table and being able to select

--- carvice-frontend/src/app/components/shared/shrd-table/shrd-table.component.ts
+++ carvice-frontend/src/app/components/shared/shrd-table/shrd-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { IDataEntity } from 'src/app/interfaces/data-entities/IDataEntity';
 import { IHeaderPair } from 'src/app/interfaces/IHeaderPair';
 
@@ -7,13 +7,54 @@ import { IHeaderPair } from 'src/app/interfaces/IHeaderPair';
   templateUrl: './shrd-table.component.html',
   styleUrls: ['./shrd-table.component.scss']
 })
-export class ShrdTableComponent implements OnInit {
+export class ShrdTableComponent implements OnInit, OnChanges {
   @Input() public collection: IDataEntity[];
   @Input() public headers: IHeaderPair[];
   @Input() public selectable: boolean;
   @Input() public palette: string;
+  @Input() public pageLength: number;
+
+  @Output() public selected: EventEmitter<string[]> = new EventEmitter<string[]>();
+
+  private _pages: number[] = [];
+  private _selectedIds: string[] = [];
+
+  public get pages(): number[] {
+    return this._pages;
+  }
 
   constructor() { }
 
-  ngOnInit() { }
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['collection']) {
+      // there are changes to the collection, re-set the selected IDs
+      this._selectedIds = [];
+      this.selected.emit(this._selectedIds);
+
+      // continue with setting the pages
+      let pagesNr = this.collection.length / this.pageLength;
+      if (pagesNr % 1 !== 0) {
+        pagesNr = Math.floor(pagesNr) + 1;
+      }
+      const items: number[] = [];
+      for (let i = 1; i <= pagesNr; i++) {
+        items.push(i);
+      }
+      this._pages = items;
+    }
+  }
+
+  public onChangeSelect(event: any, dataEntityId: string) {
+    if (event.srcElement.checked) {
+      this._selectedIds.push(dataEntityId);
+    } else {
+      const index = this._selectedIds.indexOf(dataEntityId);
+      if (index > -1) {
+        this._selectedIds.splice(index, 1);
+      }
+    }
+    this.selected.emit(this._selectedIds);
+  }
+
+  public ngOnInit(): void { }
 }
